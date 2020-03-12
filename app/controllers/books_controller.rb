193121b64f098a07c0da9412c2450e5ecc5f3054
稲葉晃平 <ko-hei@inabaakiratairanoMacBook-Pro.local>
2020-03-12 17:38:35 +0900

    [finish] 5-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
   	@books = Book.all
-  	@book=Book.new
+  	@book = Book.new
   end
 
   def show
@@ -17,17 +17,27 @@ class BooksController < ApplicationController
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to books_path
+    if book.save
+       flash[:notice] = "Book was successfully created."
+  	   redirect_to book_path(book.id)
+    else
+       flash[:notice] = "can't be blank"
+       redirect_to books_path
+    end
+
   end
 
   def update
-  	book = Blog.find(params[:id])
-    blog.update(book_params)
-    redirect_to book_path(blog)
+  	book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
